fb60bd2565bea9bc54562ea844ad76a354667f2d
IsaacWilliamson <isaacmarkwilliamson@gmail.com>
2019-08-21 12:58:57 +0100

    Style update, refactored auth services and navigation to there own classes.

--- Components/AppComponents/Profile.js
+++ Components/AppComponents/Profile.js
@@ -4,6 +4,7 @@ import firebaseConfig from "../../services/firebaseAPI";
 import {Body, Header, Icon, Left, Right, Title} from "native-base";
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from "expo-permissions";
+import blankUser from "./images/user.png";
 
 
 const {width: WIDTH} = Dimensions.get("window");
@@ -15,21 +16,31 @@ export default class Profile extends React.Component {
         this.state = {
             photoURI: null,
             currentUser: null,
-            loadImage: ''
+            loadImage: '',
+            url: '',
+            change: false
         }
     };
 
+    getImage = async (currentUser) => {
+        const ref = firebaseConfig.storage().ref('images/' + currentUser);
+        const url = await ref.getDownloadURL();
+        return url
+    };
     static navigationOptions = {
         drawerIcon: ({tintColor}) => (
             <Icon name="person" styles={{fontSize: 15, active: tintColor}}/>
         )
     };
 
-
     componentWillMount() {
         const {currentUser} = firebaseConfig.auth();
-        this.setState({currentUser})
+        this.setState({
+            currentUser,
+        })
+
     }
+
 
     async checkCameraRollPermission() {
         const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
@@ -67,7 +78,7 @@ export default class Profile extends React.Component {
         }
     };
 
-    uploadFirebase = async (uri, imageName) => {
+    uploadFirebase = async (uri) => {
         const response = await fetch(uri);
         const blob = await response.blob();
         var storageRef = firebaseConfig.storage().ref();
@@ -76,14 +87,19 @@ export default class Profile extends React.Component {
     };
 
 
-
     render() {
         const {currentUser} = this.state;
+        this.getImage(currentUser.uid).then(result => this.setState({
+            url: result
+        })).catch((err) => {
+            console.log("No")
+        });
         return (
             <View style={styles.container}>
+
                 <Header style={styles.header}>
                     <Left>
-                        <Icon name={"ios-arrow-back"} style={styles.menuIcon}/>
+                        <Icon name={"ios-add-circle-outline"} style={styles.menuIcon}/>
                     </Left>
                     <Body>
                         <Title style={styles.titleText}>Profile</Title>
@@ -97,17 +113,19 @@ export default class Profile extends React.Component {
                 </Header>
 
                 <View style={styles.container}>
-                    <View style={styles.border}></View>
-                    <Image style={styles.avatar} source={{uri: undefined}}/>
-                    <Button title="Choose Photo" onPress={this.selectImage}/>
+                    <Image style={styles.avatar} source={this.state.url ? {uri: this.state.url} : blankUser}/>
                     <View style={styles.bodyContent}>
-                        <Text style={styles.name}>John Doe</Text>
-                        <Text style={{fontSize: 20}}>Email:</Text>
-                        <TextInput style={{color: '#298158', fontSize: 20}}>
-                            {currentUser && currentUser.email}!</TextInput>
+                        <Button title="Choose Photo" onPress={this.selectImage}/>
+                        <Text style={styles.name}>BORIS BOYS</Text>
+                        <Text style={styles.info}>Email:</Text>
+                        <TextInput editable={this.state.change} style={{color: '#298158', fontSize: 20}}>
+                            {currentUser.email}!</TextInput>
                         <Text style={styles.info}>Username</Text>
-                        <TextInput style={{color: '#298158', fontSize: 20}}>
-                            {currentUser && currentUser.uid}!</TextInput>
+                        <TextInput editable={this.state.change} style={{color: '#298158', fontSize: 20}}>
+                            {currentUser.uid}!</TextInput>
+                        <Text style={styles.info}>Password</Text>
+                        <TextInput editable={this.state.change} style={{color: '#298158', fontSize: 20}}>
+                            *******</TextInput>
 
                     </View>
                 </View>
@@ -122,14 +140,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    colour: {
+        backgroundColor: "#243060",
+        height: 100,
+    },
     backgroundContainer: {
         width: null,
-        height: null,
-        flex: 1,
         backgroundColor: "#EEF5DB"
     },
     header: {
-        backgroundColor: "#60A7BF",
+        backgroundColor: "#243060",
         alignItems: "center",
         textAlignVertical: "center",
         color: "rgba(255,255,255,255.7)",
@@ -149,15 +169,16 @@ const styles = StyleSheet.create({
         height: 130,
         borderRadius: 63,
         borderWidth: 2,
-        borderColor: "#4f6367",
+        borderColor: "rgba(255,255,255, 255)",
         marginBottom: 10,
         alignSelf: 'center',
         position: 'absolute',
-        marginTop: 60
+        marginTop: 60,
+        backgroundColor: "#243060",
     },
     name: {
         fontSize: 22,
-        color: "#FFFFFF",
+        color: "#4f6367",
         fontWeight: '600',
     },
     bodyContent: {
